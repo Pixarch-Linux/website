bc11e74e1b8a34cc1a50973a5b8c1a73a2da448a
haard <Haardispro>
2025-03-05 00:30:06 +0530

    rebuild the site in flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for 
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/download')
+def download():
+    return render_template('downloads.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
